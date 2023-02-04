2bdb7522fee1a172ee2f70beb80d71761011e9ef
Max Link <max.durham.link@gmail.com>
2023-02-04 14:19:04 -0600

    MySortClass shellsort function

--- MySortClass.cpp
+++ MySortClass.cpp
@@ -3,13 +3,34 @@
 
 void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
     //shellsort pseudo code 
-    for code = 1 to p 
-        h = hlist[code]
-        for j = 2 to n 
-            i = j 
-            while (i > 1) and (A[i-h] > A[i])
-                swap(A[i], A[i-h])
-                i = i - h 
+    int gap = code / 2; 
+
+    while(gap > 0){
+
+        int j = 0; 
+
+        //TODO - get s to be the array. look through the object functions 
+
+        for(int i = gap; i < code; i++){
+
+            int temp = s[i]; 
+
+            for(j = i; j >= gap && s[i-gap] > temp; j-=gap){
+
+                s[j] = s[j - gap]; 
+
+            }
+
+            s[j] = temp; 
+
+        }
+
+
+        gap = gap / 2; 
+
+
+
+    }
 
 
 }
