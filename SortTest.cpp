03443108a221f2fbb84b1d763811b3003ce72425
Max Link <max.durham.link@gmail.com>
2023-02-04 16:40:04 -0600

    added shellsort function in main to mess with

--- SortTest.cpp
+++ SortTest.cpp
@@ -15,6 +15,29 @@ void insertionSort(SortClass &s) //sort the items in s by insertion sort
 	} 
 }
 
+
+int shellSortTest(int arr[], int n){
+    for(int gap = n/2; gap > 0; gap /= 2){
+        for(int i = gap; i < n; i += 1){
+            int temp = arr[i]; 
+
+            int j; 
+            for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
+                arr[j] = arr[j-gap]; 
+            }
+            arr[j] = temp; 
+        }
+    }
+
+    return 0; 
+}
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " "; 
+    }
+}
+
 int main()
 {
 
@@ -28,7 +51,21 @@ int main()
     cout << "Swap count : " << st.getSwapCount() << endl;
 
     cout << "ShellSort: " << endl; 
-    ShellSort(st, 1); 
+    //ShellSort(st, 1); //HOW TO CALL? 
+
+    cout << "-----ShellSort Test------" << endl; 
+
+    int arr[] = {12, 34, 54, 2, 3}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+
+    cout << "Array before sorting: " << "\n"; 
+    printArray(arr, n); 
+
+    shellSortTest(arr, n); //shellsort function above 
+
+    cout << "\narray after sorting \n"; 
+
+    printArray(arr, n); 
    
     return 0;
 }
