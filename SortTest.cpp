af4bf30c06a60dc34a6562ca6a8c87c375ea7801
MaxDLink <max.durham.link@gmail.com>
2023-02-05 13:05:36 -0600

    knuth formula

--- SortTest.cpp
+++ SortTest.cpp
@@ -58,8 +58,9 @@ int shellSortTest(int arr[], int n){
 
 void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
     //[20, 5, 40, 60, 10, 30] <-- array 
-    for (int k = 1; k < s.count(); k++){
-        int h = hlist[k]; //TODO - what is hlist? Knuth's formula
+    for (int k = 0; k < s.count(); k++){
+        int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
+        cout << "KNUTH FORMULA: " << h << endl; 
         for (int j = 1; j < s.count(); j++) 
 	{
 	   int i = j; //sets i = 1. Here, j and i both have index 1 [5]
