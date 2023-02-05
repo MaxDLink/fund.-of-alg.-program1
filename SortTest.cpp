43b32f13b3e751bdec0ef72675b8f703b60450d8
MaxDLink <max.durham.link@gmail.com>
2023-02-05 10:56:53 -0600

    added insertion sort test code

--- SortTest.cpp
+++ SortTest.cpp
@@ -2,6 +2,29 @@
 #include "SortClass.h"
 
 
+// Function to sort an array using
+// insertion sort
+void insertionSortTest(int arr[], int n) //takes in array and size of array 
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+ 
+        // Move elements of arr[0..i-1], 
+        // that are greater than key, to one
+        // position ahead of their
+        // current position
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 void insertionSort(SortClass &s) //sort the items in s by insertion sort 
 {
     for (int j = 1; j < s.count(); j++)
@@ -50,21 +73,28 @@ int main()
     cout << st << endl; 
     cout << "Swap count : " << st.getSwapCount() << endl;
 
-    cout << "ShellSort: " << endl; 
+    // cout << "ShellSort: " << endl; 
     //ShellSort(st, 1); //HOW TO CALL? 
 
-    cout << "-----ShellSort Test------" << endl; 
+    // cout << "-----ShellSort Test------" << endl; 
 
     int arr[] = {12, 34, 54, 2, 3}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
 
-    cout << "Array before sorting: " << "\n"; 
+    // cout << "Array before sorting: " << "\n"; 
+    // printArray(arr, n); 
+
+    // shellSortTest(arr, n); //shellsort function above 
+
+    // cout << "\narray after sorting \n"; 
+
+    // printArray(arr, n); 
+
+    cout << "Insertion Sort Test: " << endl; 
+    cout << "Array Before sorting: " << endl; 
     printArray(arr, n); 
-
-    shellSortTest(arr, n); //shellsort function above 
-
     cout << "\narray after sorting \n"; 
-
+    insertionSortTest(arr, n); 
     printArray(arr, n); 
    
     return 0;
