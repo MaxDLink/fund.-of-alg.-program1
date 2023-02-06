d85fd8654dee36256165a4beadb8591efafc966f
MaxDLink <max.durham.link@gmail.com>
2023-02-05 20:33:43 -0600

    moved ShellSort to MySortClass.cpp

--- MySortClass.cpp
+++ MySortClass.cpp
@@ -1,22 +1,86 @@
 #include <iostream> 
 #include "SortClass.h"
 
-void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
-    //shellsort pseudo code 
+#include "vector" //TODO - put in own function? 
+#include <cmath> //TODO - put in own function? 
+#include <algorithm> //TODO - put in own function? 
 
-for(int k = 1; k < s.count(); k++){
-    int h = code; 
- //insertion sort 
-    for (int j = 1; j < s.count(); j++)
-	{
-	   int i = j;
-	   while ((i >= 0) && (s.lessthan(i, i-h)))
-		{
-		   s.swap(i-h, i);
-		   i = i - h; 
-		}
-	} 
-}
-   
+void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
+ if(code > 3){//exits program if code is not 0, 1, 2, 3
+        cout << "Code was not 0, 1, 2, or 3. Exiting program!" << endl; 
+        return; //returns to exit function; 
+    }
+    else{//runs if code is 0, 1, 2, 3 
+            //[20, 5, 40, 60, 10, 30] <-- array 
+       // for (int k = 0; k < s.count(); k++){ //loops through code int. 
+            // int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
+            int h = 0; 
+            vector <int> hlist; //empty hlist vector 
+            if(code == 0){//h = 1 
+                h = 1; //h is 1 so program does insertion sort 
+            }if(code == 1){//h = k^2
+                int g = 1; 
+                int l = 0; 
+                while(g < s.count()){
+                    l = g * g; 
+                    hlist.push_back(l); 
+                    g++; 
+                }
+                 //print vector 
+                reverse(hlist.begin(), hlist.end());
+                cout << "HList: " << endl; 
+                    for(int k = 0; k < hlist.size(); k++){
+                        cout << hlist.at(k) << " "; 
+                    }
+                cout << endl; 
+
+            }if(code == 2){//h = 2^k -1. hibbard sequence. 
+                int l = 0; 
+                int g = 0; 
+                while(g < s.count()){
+                    //cout << "S: " << s.count() << endl; 
+                    l += 1; 
+                    //cout << "L: " << l << endl; 
+                    g = (pow(2, l)) - 1; 
+                    //cout << "\nG before push: " << g << endl; 
+                    hlist.push_back(g); 
+                }
+                //print vector 
+                reverse(hlist.begin(), hlist.end());
+                cout << "HList: " << endl; 
+                    for(int k = 0; k < hlist.size(); k++){
+                        cout << hlist.at(k) << " "; 
+                    }
+                cout << endl; 
+            }if(code == 3){//h = (3^k-1)/2
+                int l = 0; 
+                int g = 0; 
+                while(g < s.count()){
+                    l += 1; 
+                    g = ((pow(3, l)) -1)/2; 
+
+                    hlist.push_back(g); 
+
+                }
+                //print vector 
+                reverse(hlist.begin(), hlist.end());
+                cout << "HList: " << endl; 
+                    for(int k = 0; k < hlist.size(); k++){
+                        cout << hlist.at(k) << " "; 
+                    }
+                cout << endl; 
+            }
+            for (int j = 1; j < s.count(); j++) 
+        {
+        int i = j; //sets i = 1. Here, j and i both have index 1 [5]
+        while ((i >= 0) && (s.lessthan(i, i-h))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
+            {
+                s.swap(i-h, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
+                i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
+            }
+        } 
+        //}
+    }
+    
 
 }
--- SortTest.cpp
+++ SortTest.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "SortClass.h"
-#include "vector" //TODO - take out vector and add to MySortClass later 
-#include <cmath> //TODO - take out cmath and add to MySortClass later 
-#include <algorithm> //TODO - take out reverse and add to MySortClass later 
+// #include "vector" //TODO - take out vector and add to MySortClass later 
+// #include <cmath> //TODO - take out cmath and add to MySortClass later 
+// #include <algorithm> //TODO - take out reverse and add to MySortClass later 
 // Function to sort an array using
 // insertion sort
 void insertionSortTest(int arr[], int n) //takes in array and size of array 
@@ -57,85 +57,85 @@ int shellSortTest(int arr[], int n){
     return 0; 
 }
 
-void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
- if(code > 3){//exits program if code is not 0, 1, 2, 3
-        cout << "Code was not 0, 1, 2, or 3. Exiting program!" << endl; 
-        return; //returns to exit function; 
-    }
-    else{//runs if code is 0, 1, 2, 3 
-            //[20, 5, 40, 60, 10, 30] <-- array 
-       // for (int k = 0; k < s.count(); k++){ //loops through code int. 
-            // int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
-            int h = 0; 
-            vector <int> hlist; //empty hlist vector 
-            if(code == 0){//h = 1 
-                h = 1; //h is 1 so program does insertion sort 
-            }if(code == 1){//h = k^2
-                int g = 1; 
-                int l = 0; 
-                while(g < s.count()){
-                    l = g * g; 
-                    hlist.push_back(l); 
-                    g++; 
-                }
-                 //print vector 
-                reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
+// void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
+//  if(code > 3){//exits program if code is not 0, 1, 2, 3
+//         cout << "Code was not 0, 1, 2, or 3. Exiting program!" << endl; 
+//         return; //returns to exit function; 
+//     }
+//     else{//runs if code is 0, 1, 2, 3 
+//             //[20, 5, 40, 60, 10, 30] <-- array 
+//        // for (int k = 0; k < s.count(); k++){ //loops through code int. 
+//             // int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
+//             int h = 0; 
+//             vector <int> hlist; //empty hlist vector 
+//             if(code == 0){//h = 1 
+//                 h = 1; //h is 1 so program does insertion sort 
+//             }if(code == 1){//h = k^2
+//                 int g = 1; 
+//                 int l = 0; 
+//                 while(g < s.count()){
+//                     l = g * g; 
+//                     hlist.push_back(l); 
+//                     g++; 
+//                 }
+//                  //print vector 
+//                 reverse(hlist.begin(), hlist.end());
+//                 cout << "HList: " << endl; 
+//                     for(int k = 0; k < hlist.size(); k++){
+//                         cout << hlist.at(k) << " "; 
+//                     }
+//                 cout << endl; 
 
-            }if(code == 2){//h = 2^k -1. hibbard sequence. 
-                int l = 0; 
-                int g = 0; 
-                while(g < s.count()){
-                    //cout << "S: " << s.count() << endl; 
-                    l += 1; 
-                    //cout << "L: " << l << endl; 
-                    g = (pow(2, l)) - 1; 
-                    //cout << "\nG before push: " << g << endl; 
-                    hlist.push_back(g); 
-                }
-                //print vector 
-                reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
-            }if(code == 3){//h = (3^k-1)/2
-                int l = 0; 
-                int g = 0; 
-                while(g < s.count()){
-                    l += 1; 
-                    g = ((pow(3, l)) -1)/2; 
+//             }if(code == 2){//h = 2^k -1. hibbard sequence. 
+//                 int l = 0; 
+//                 int g = 0; 
+//                 while(g < s.count()){
+//                     //cout << "S: " << s.count() << endl; 
+//                     l += 1; 
+//                     //cout << "L: " << l << endl; 
+//                     g = (pow(2, l)) - 1; 
+//                     //cout << "\nG before push: " << g << endl; 
+//                     hlist.push_back(g); 
+//                 }
+//                 //print vector 
+//                 reverse(hlist.begin(), hlist.end());
+//                 cout << "HList: " << endl; 
+//                     for(int k = 0; k < hlist.size(); k++){
+//                         cout << hlist.at(k) << " "; 
+//                     }
+//                 cout << endl; 
+//             }if(code == 3){//h = (3^k-1)/2
+//                 int l = 0; 
+//                 int g = 0; 
+//                 while(g < s.count()){
+//                     l += 1; 
+//                     g = ((pow(3, l)) -1)/2; 
 
-                    hlist.push_back(g); 
+//                     hlist.push_back(g); 
 
-                }
-                //print vector 
-                reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
-            }
-            for (int j = 1; j < s.count(); j++) 
-        {
-        int i = j; //sets i = 1. Here, j and i both have index 1 [5]
-        while ((i >= 0) && (s.lessthan(i, i-h))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
-            {
-                s.swap(i-h, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
-                i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
-            }
-        } 
-        //}
-    }
+//                 }
+//                 //print vector 
+//                 reverse(hlist.begin(), hlist.end());
+//                 cout << "HList: " << endl; 
+//                     for(int k = 0; k < hlist.size(); k++){
+//                         cout << hlist.at(k) << " "; 
+//                     }
+//                 cout << endl; 
+//             }
+//             for (int j = 1; j < s.count(); j++) 
+//         {
+//         int i = j; //sets i = 1. Here, j and i both have index 1 [5]
+//         while ((i >= 0) && (s.lessthan(i, i-h))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
+//             {
+//                 s.swap(i-h, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
+//                 i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
+//             }
+//         } 
+//         //}
+//     }
     
 
-}
+// }
 
 // void printArray(int arr[], int n){
 //     for(int i = 0; i < n; i++){
