49ece79223d58fbf383cf7b94d102e4d3bb5e4d8
MaxDLink <max.durham.link@gmail.com>
2023-02-05 20:52:41 -0600

    messing with SortShell logic

--- MySortClass.cpp
+++ MySortClass.cpp
@@ -5,6 +5,17 @@
 #include <cmath> //TODO - put in own function? 
 #include <algorithm> //TODO - put in own function? 
 
+using namespace std; 
+
+
+void printVector(vector <int> vect){
+	cout << "HList: " << endl; 
+    for(int i = 0; i < vect.size(); i++){
+        cout << vect.at(i) << " "; 
+    }
+	cout << endl; 
+}
+
 void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the SortClass object, code: an integer denoting how the hlist array is to be formed (0, 1, 2, 3, print error message & exit program)
  if(code > 3){//exits program if code is not 0, 1, 2, 3
         cout << "Code was not 0, 1, 2, or 3. Exiting program!" << endl; 
@@ -12,12 +23,12 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
     }
     else{//runs if code is 0, 1, 2, 3 
             //[20, 5, 40, 60, 10, 30] <-- array 
-       // for (int k = 0; k < s.count(); k++){ //loops through code int. 
+       for (int k = 0; k < s.count(); k++){ //loops through code int. 
             // int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
-            int h = 0; 
-            vector <int> hlist; //empty hlist vector 
+            vector <int> hlist; //empty hlist vector
+			int h = 0;  
             if(code == 0){//h = 1 
-                h = 1; //h is 1 so program does insertion sort 
+                hlist[0] = 1; //h is 1 so program does insertion sort 
             }if(code == 1){//h = k^2
                 int g = 1; 
                 int l = 0; 
@@ -28,11 +39,8 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 }
                  //print vector 
                 reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
+              
+				printVector(hlist); 
 
             }if(code == 2){//h = 2^k -1. hibbard sequence. 
                 int l = 0; 
@@ -47,11 +55,8 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 }
                 //print vector 
                 reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
+             
+				printVector(hlist); 
             }if(code == 3){//h = (3^k-1)/2
                 int l = 0; 
                 int g = 0; 
@@ -64,12 +69,11 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 }
                 //print vector 
                 reverse(hlist.begin(), hlist.end());
-                cout << "HList: " << endl; 
-                    for(int k = 0; k < hlist.size(); k++){
-                        cout << hlist.at(k) << " "; 
-                    }
-                cout << endl; 
+
+				printVector(hlist); 
             }
+			h = hlist[k]; //sets your h value for swap below
+			// cout << "H: " << h << endl;  
             for (int j = 1; j < s.count(); j++) 
         {
         int i = j; //sets i = 1. Here, j and i both have index 1 [5]
@@ -79,8 +83,9 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
             }
         } 
-        //}
+       } //end of for loop for hlist array 
     }
     
 
 }
+
