f257a5c276c28710d5627b8138b29f44e50ab280
MaxDLink <max.durham.link@gmail.com>
2023-02-06 11:12:50 -0600

    fixed code == 0 by giving all of hlist 1

--- MySortClass.cpp
+++ MySortClass.cpp
@@ -23,13 +23,15 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
     }
     else{//runs if code is 0, 1, 2, 3 
             //[20, 5, 40, 60, 10, 30] <-- array 
-       for (int k = 0; k < s.count()-1; k++){ //loops through code int. 
-            // int h = 3 * k + 1; //TODO - what is hlist? Knuth's formula
+       for (int k = 0; k < s.count(); k++){ //loops through code int. 
             vector <int> hlist; //empty hlist vector
 			int h = 0;  
             if(code == 0){//h = 1 
-              // hlist[0] = 1; //h is 1 so program does insertion sort 
-			   h = 1; 
+                int g = 0; 
+                while(g < s.count()){//pushes a 1 to every array spot 
+                    hlist.push_back(1); //h is 1 so program does insertion sort 
+                    g++; 
+                }
             }if(code == 1){//h = k^2
                 int g = 1; 
                 int l = 0; 
@@ -39,12 +41,7 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                     g++; 
                 }
                  //print vector 
-                reverse(hlist.begin(), hlist.end());
-              
-				//printVector(hlist);
-
-				h = hlist[k]; //sets your h value for swap below 
-				
+                reverse(hlist.begin(), hlist.end());	
 
             }if(code == 2){//h = 2^k -1. hibbard sequence. 1, 3, 7, 15, 31
                 int l = 0; 
@@ -59,16 +56,7 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 }
                 //print vector 
                 reverse(hlist.begin(), hlist.end());
-             
-				// printVector(hlist); 
-				//TODO - k is accessing null indexes???? Should only have 5 filled out indexes and stop at 1 to do insertion sort. 
-				//K accessing null indexes is reason for seg fault 
-				h = hlist[k]; //sets your h value for swap below
-				
-				
-			
-			
-				
+		
             }if(code == 3){//h = (3^k-1)/2
                 int l = 0; 
                 int g = 0; 
@@ -81,22 +69,18 @@ void ShellSort(SortClass& s, int code){//implementation of ShellSort - s: the So
                 }
                 //print vector 
                 reverse(hlist.begin(), hlist.end());
-
-				// printVector(hlist); 
-
-				h = hlist[k]; //sets your h value for swap below
 				
             }
-			// h = hlist[k]; //sets your h value for swap below
-			//cout << "H: " << h << endl;  
-            for (int j = 1; j < s.count(); j++) 
-        {
-        int i = j; //sets i = 1. Here, j and i both have index 1 [5]
-        while ((i >= 0) && (s.lessthan(i, i-h))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
-            {
-                s.swap(i-h, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
-                i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
-            }
+
+			h = hlist[k]; //sets your h value for swap below
+			cout << "H: " << h << endl;  
+        for (int j = 1; j < s.count(); j++) {
+            int i = j; //sets i = 1. Here, j and i both have index 1 [5]
+            while ((i >= 0) && (s.lessthan(i, i-h))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
+                {
+                    s.swap(i-h, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
+                    i = i - h; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
+                }
         } 
       } //end of for loop for hlist array 
     }
