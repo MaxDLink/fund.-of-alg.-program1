c5f7d90fa1a991dbd3e666a1c2c707502185e3d0
MaxDLink <max.durham.link@gmail.com>
2023-02-09 10:10:27 -0600

    simplified SortTest code

--- SortTest.cpp
+++ SortTest.cpp
@@ -7,26 +7,26 @@
 //TODO - how will main work with the default code? 
 // Function to sort an array using
 // insertion sort
-void insertionSortTest(int arr[], int n) //takes in array and size of array 
-{
-    int i, key, j; //create 3 integers 
-    for (i = 1; i < n; i++)//outer for loop deals with i 
-    {
-        key = arr[i]; //key is set to element at specific i to compare to every single j element. 
-        j = i - 1; //set j to one i before key so that you can compare key to arr[j]
+// void insertionSortTest(int arr[], int n) //takes in array and size of array 
+// {
+//     int i, key, j; //create 3 integers 
+//     for (i = 1; i < n; i++)//outer for loop deals with i 
+//     {
+//         key = arr[i]; //key is set to element at specific i to compare to every single j element. 
+//         j = i - 1; //set j to one i before key so that you can compare key to arr[j]
  
-        // Move elements of arr[0..i-1], 
-        // that are greater than key, to one
-        // position ahead of their
-        // current position
-        while (j >= 0 && arr[j] > key)//when arr[j] element is greater than key element 
-        {
-            arr[j + 1] = arr[j]; //changes the element after arr[j] to arr[j]. Moves your arr[j] up one element to take key's spot 
-            j = j - 1; //decrement j by one so that j is pointing to one element before arr[j]. 
-        }
-        arr[j + 1] = key; //move key element to its sorted place in the array 
-    }
-}
+//         // Move elements of arr[0..i-1], 
+//         // that are greater than key, to one
+//         // position ahead of their
+//         // current position
+//         while (j >= 0 && arr[j] > key)//when arr[j] element is greater than key element 
+//         {
+//             arr[j + 1] = arr[j]; //changes the element after arr[j] to arr[j]. Moves your arr[j] up one element to take key's spot 
+//             j = j - 1; //decrement j by one so that j is pointing to one element before arr[j]. 
+//         }
+//         arr[j + 1] = key; //move key element to its sorted place in the array 
+//     }
+// }
 
 void insertionSort(SortClass &s) //sort the items in s by insertion sort 
 {
@@ -37,42 +37,44 @@ void insertionSort(SortClass &s) //sort the items in s by insertion sort
 	   while ((i >= 0) && (s.lessthan(i, i-1))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
 		{
 		   s.swap(i-1, i); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20. 
+           cout << "SWAPPED." << " List is now: " << s << endl;  //TODO - take out once done comparing shellsort & insertion sort 
 		   i--; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
 		}
 	} 
 }
 
 
-int shellSortTest(int arr[], int n){
-    for(int gap = n/2; gap > 0; gap /= 2){
-        for(int i = gap; i < n; i += 1){
-            int temp = arr[i]; 
+// int shellSortTest(int arr[], int n){
+//     for(int gap = n/2; gap > 0; gap /= 2){
+//         for(int i = gap; i < n; i += 1){
+//             int temp = arr[i]; 
 
-            int j; 
-            for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
-                arr[j] = arr[j-gap]; 
-            }
-            arr[j] = temp; 
-        }
-    }
+//             int j; 
+//             for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
+//                 arr[j] = arr[j-gap]; 
+//             }
+//             arr[j] = temp; 
+//         }
+//     }
 
-    return 0; 
-}
+//     return 0; 
+// }
 
-// int calculatedShellSortTest(int arr[], int n, int code){
+
+// int ShellSortObject(SortClass& s, int code){
 //     int gap = 0; 
 //     vector <int> hlist; 
 //     if(code == 0){
 //         hlist.push_back(1); 
 //     }
-//     if(code == 1){
+//     if(code == 1){//sequence: 1, 4, 9
 //         int g = 1; 
 //                 int l = 0; 
-//                 while(l < n){
+//                 while(l < s.count()){
 //                     l = g * g; 
 //                     hlist.push_back(l); 
 //                     g++; 
-//                     l = g * g; //TODO - need this line? 
+//                     //l = g * g; //TODO - need this line? 
 //                 }
 //                  //print vector 
 //                 reverse(hlist.begin(), hlist.end());
@@ -80,7 +82,7 @@ int shellSortTest(int arr[], int n){
 //     if(code == 2){
 //          int l = 0; 
 //         int g = 0; 
-//         while(g < n){//stops at h = 31 //g = 7 stop at 7 
+//         while(g < s.count()){//stops at h = 31 //g = 7 stop at 7 
 //                     //cout << "S: " << s.count() << endl; 
 //                     l += 1; 
 //                     //cout << "L: " << l << endl; 
@@ -94,7 +96,7 @@ int shellSortTest(int arr[], int n){
 //     if(code == 3){
 //         int l = 0; 
 //                 int g = 0; 
-//                 while(g < n){
+//                 while(g < s.count()){
 //                     l += 1; 
 //                     g = ((pow(3, l)) -1)/2; 
 
@@ -106,89 +108,28 @@ int shellSortTest(int arr[], int n){
 //     }
 //     for(int count = 0; count < hlist.size(); count++){
 //         gap = hlist[count]; //assigns gap 
-//         for(int i = gap; i < n; i += 1){
-//             int temp = arr[i]; 
+//         for(int i = gap; i < s.count(); i += 1){
+//             // int temp = arr[i]; 
 
-//             int j; 
-//             for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
-//                 arr[j] = arr[j-gap]; 
-//             }
-//             arr[j] = temp; 
+//             // int j; 
+//             // for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
+//             //     arr[j] = arr[j-gap]; 
+//             // }
+//             // arr[j] = temp; 
+//             //TODO - replace the above main logic code for swapping with the below and debug 
+//             cout << "GAP: " << gap << endl; 
+//             int j = i; //sets i = 1. Here, j and i both have index 1 [5]
+//             cout << "J: " << j << endl; 
+//                 while ((j >= 0) && (s.lessthan(j, j-gap))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
+//                     {
+//                         s.swap(j-gap, j); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20.
+//                         cout << "SWAPPED." << " List is now: " << s << endl;  
+//                         j = j - gap; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
+//                     }
 //         }
 //     } //end of hlist for loop 
 //     return 0; 
 // }
-
-
-int ShellSortObject(SortClass& s, int code){
-    int gap = 0; 
-    vector <int> hlist; 
-    if(code == 0){
-        hlist.push_back(1); 
-    }
-    if(code == 1){//sequence: 1, 4, 9
-        int g = 1; 
-                int l = 0; 
-                while(l < s.count()){
-                    l = g * g; 
-                    hlist.push_back(l); 
-                    g++; 
-                    //l = g * g; //TODO - need this line? 
-                }
-                 //print vector 
-                reverse(hlist.begin(), hlist.end());
-    }
-    if(code == 2){
-         int l = 0; 
-        int g = 0; 
-        while(g < s.count()){//stops at h = 31 //g = 7 stop at 7 
-                    //cout << "S: " << s.count() << endl; 
-                    l += 1; 
-                    //cout << "L: " << l << endl; 
-                    g = (pow(2, l)) - 1; 
-                    //cout << "\nG before push: " << g << endl; 
-                    hlist.push_back(g);
-                }
-                //print vector 
-                reverse(hlist.begin(), hlist.end()); //7, 3, 1        
-    }
-    if(code == 3){
-        int l = 0; 
-                int g = 0; 
-                while(g < s.count()){
-                    l += 1; 
-                    g = ((pow(3, l)) -1)/2; 
-
-                    hlist.push_back(g); 
-
-                }
-                //print vector 
-                reverse(hlist.begin(), hlist.end());
-    }
-    for(int count = 0; count < hlist.size(); count++){
-        gap = hlist[count]; //assigns gap 
-        for(int i = gap; i < s.count(); i += 1){
-            // int temp = arr[i]; 
-
-            // int j; 
-            // for(j = i; j >= gap && arr[j-gap] > temp; j -= gap){
-            //     arr[j] = arr[j-gap]; 
-            // }
-            // arr[j] = temp; 
-            //TODO - replace the above main logic code for swapping with the below and debug 
-            cout << "GAP: " << gap << endl; 
-            int j = i; //sets i = 1. Here, j and i both have index 1 [5]
-            cout << "J: " << j << endl; 
-                while ((j >= 0) && (s.lessthan(j, j-gap))) //less than takes in 1, and 0. comparing 5 to 20 and 5 < 20 true 
-                    {
-                        s.swap(j-gap, j); //0 and 1. Now array is: [5, 20, 40, 60, 10, 30]. just swapped 5 and 20.
-                        cout << "SWAPPED." << " List is now: " << s << endl;  
-                        j = j - gap; //decrement i by 1 so that i is 0 and the while loop breaks in the less than comparison 
-                    }
-        }
-    } //end of hlist for loop 
-    return 0; 
-}
 
 int main()
 {
@@ -197,8 +138,8 @@ int main()
     SortClass unsorted = st; //stores unsorted st //TODO - use copy constructor in MySortClass.cpp with another sortclass object 
 
     cout << "Shell Sort Object: " << endl; 
-    cout << "Unsorted: " << unsorted << endl; 
-    ShellSort(unsorted, 1); 
+    cout << "Unsorted --- Shell Sort ----: " << unsorted << endl; 
+    ShellSort(unsorted, 0); 
     cout << "Sorted: " << unsorted << endl; 
     cout << "Swap count : " << unsorted.getSwapCount() << endl; 
 
@@ -206,7 +147,7 @@ int main()
 //TODO - can use copy constructor to copy sort class to another object? 
 //TODO - have to sort the same set 4 different times with code 0, 1, 2, 3 
 
-    cout << "\nUNSORTED: " << st << endl; 
+    cout << "\nUNSORTED --- Insertion Sort ----: " << st << endl; 
     insertionSort(st); //call insertion sort 
     cout << "------\n";
     cout << "Insertion sort: " << endl; 
